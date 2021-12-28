272e75d69caa5019499c9370cc91edeca29c354c
mhuap <huapmar97@gmail.com>
2021-12-27 22:59:28 -0500

    PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+const prod = process.env.NODE_ENV === 'production';
+
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
   reactStrictMode: true,
-}
+  pwa: {
+    dest: 'public',
+    disable: prod ? false : true
+  }
+})
