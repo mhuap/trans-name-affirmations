ee68e501bda5cbe4e286da47bb2ad54eac7d3ea5
mhuap <huapmar97@gmail.com>
2021-12-28 14:23:02 -0500

    switch branch to develop

---
+++ firebase/webPush.js
@@ -0,0 +1,71 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import localforage from 'localforage';
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+
+const clientCredentials = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// const firebaseCloudMessaging = {
+//   tokenInlocalforage: async () => {
+//     return localforage.getItem('fcm_token')
+//   },
+//
+//   init: async function (setTokenFound) {
+//
+//     try {
+//       const currentToken = await this.tokenInlocalforage()
+//       if (currentToken !== null) {
+//         console.log('current token for client ', currentToken);
+//         setTokenFound(true);
+//         return false
+//       }
+//
+//       await Notification.requestPermission()
+//       const token = await getToken(messaging, { vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY })
+//
+//       localforage.setItem('fcm_token', token)
+//       console.log('fcm_token', token)
+//     } catch (error) {
+//       console.log('An error occurred while retrieving token. ', error);
+//       console.error(error)
+//     }
+//   },
+// }
+
+const app = initializeApp(clientCredentials);
+// const analytics = getAnalytics(app);
+const messaging = getMessaging(app);
+
+const getToken2 = async (setTokenFound) => {
+  let currentToken = "";
+
+  try {
+    currentToken = await getToken(messaging, { vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY });
+    if (currentToken) {
+      setTokenFound(true);
+    } else {
+      setTokenFound(false);
+    }
+  } catch (error) {
+    console.log("An error occurred while retrieving token. ", error);
+  }
+
+  return currentToken;
+};
+
+const onMessageListener = () =>
+  new Promise((resolve) => {
+    onMessage(messaging, (payload) => {
+      resolve(payload);
+    });
+})
+
+export { getToken2, onMessageListener };
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
-import { useRef, useState } from "react";
-import Head from 'next/head'
-import Image from 'next/image'
+import { useRef, useState, useEffect } from "react";
+import { getToken2, onMessageListener } from '../firebase/webPush.js';
+import IndexHead from '../components/indexHead.js'
 // import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const [name, setName] = useState(null)
   const nameInput = useRef(null);
+
+  const [isTokenFound, setTokenFound] = useState(false);
+  const [show, setShow] = useState(false);
+  const [notification, setNotification] = useState(true);
+
+  useEffect(() => {
+    // firebaseCloudMessaging.init(setTokenFound)
+    getToken2(setTokenFound)
+  }, [])
+
+  onMessageListener()
+    .then((payload) => {
+      setShow(true);
+      setNotification({
+        title: payload.notification.title,
+        body: payload.notification.body,
+      });
+      console.log(payload);
+    })
+    .catch((err) => console.log("failed: ", err));
+
 
   const handleSubmit = (e) => {
       e.preventDefault();
@@ -13,56 +34,18 @@ export default function Home() {
       setName(nameInput.current.value)
   }
 
+
   const message = <>
     You will now receive daily affirmations with the name
     <span className="font-bold">{' ' + name}</span>
     .
     </>
 
+
+
   return (
     <div>
-      <Head>
-        <title>Trans name affirmations</title>
-        <meta name="description" content="Generate trans name affirmations" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
-
-        <meta name='application-name' content='Trans Name Affirmations' />
-        <meta name='apple-mobile-web-app-capable' content='yes' />
-        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-        <meta name='apple-mobile-web-app-title' content='Trans Name Affirmations' />
-        <meta name='description' content='Generate trans name affirmations' />
-        <meta name='format-detection' content='telephone=no' />
-        <meta name='mobile-web-app-capable' content='yes' />
-        <meta name='msapplication-config' content='/icons/browserconfig.xml' />
-        <meta name='msapplication-TileColor' content='#2B5797' />
-        <meta name='msapplication-tap-highlight' content='no' />
-        <meta name='theme-color' content='#000000' />
-
-        <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
-        <link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' />
-        <link rel='apple-touch-icon' sizes='167x167' href='/icons/touch-icon-ipad-retina.png' />
-
-        <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
-        <link rel='manifest' href='/manifest.json' />
-        <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
-        <link rel='shortcut icon' href='/favicon.ico' />
-        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
-
-        <meta name='twitter:card' content='summary' />
-        <meta name='twitter:url' content='https://yourdomain.com' />
-        <meta name='twitter:title' content='PWA App' />
-        <meta name='twitter:description' content='Best PWA App in the world' />
-        <meta name='twitter:image' content='https://yourdomain.com/icons/android-chrome-192x192.png' />
-        <meta name='twitter:creator' content='@DavidWShadow' />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='PWA App' />
-        <meta property='og:description' content='Best PWA App in the world' />
-        <meta property='og:site_name' content='PWA App' />
-        <meta property='og:url' content='https://yourdomain.com' />
-        <meta property='og:image' content='https://yourdomain.com/icons/apple-touch-icon.png' />
-      </Head>
+      <IndexHead />
 
       <main className="container mx-auto mt-24">
         <form onSubmit={handleSubmit} className="mb-4 max-w-xs">
@@ -80,8 +63,11 @@ export default function Home() {
         </form>
 
         <p className="text-gray-700">
-          {name ? message : null}
+          {name && message}
         </p>
+
+        {isTokenFound && <h1> Notification permission enabled 👍🏻 </h1>}
+        {!isTokenFound && <h1> Need notification permission ❗️ </h1>}
       </main>
 
     </div>
