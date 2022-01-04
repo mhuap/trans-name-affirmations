e39b33382976e4d1cc383327c283dda3b920b993
mhuap <huapmar97@gmail.com>
2022-01-04 15:46:04 -0500

    Next links

--- components/content.js
+++ components/content.js
@@ -1,14 +1,15 @@
 import { AiFillSetting } from "react-icons/ai";
 import NotificationToast from './notificationToast.js';
+import Link from 'next/link';
 
 
 export default function Content({ show }) {
   return (<>
-      <a className="w-16 h-16 bg-white rounded-full mb-4 flex justify-center items-center text-3xl text-sky-300 white-drop-shadow-xl"
-        type="button"
-        href="/settings">
-        <AiFillSetting />
-      </a>
+      <Link href="/settings">
+        <a className="w-16 h-16 bg-white rounded-full mb-4 flex justify-center items-center text-3xl text-sky-300 white-drop-shadow-xl">
+          <AiFillSetting />
+        </a>
+      </Link>
 
       {!show && <p className="uppercase text-xs tracking-wide font-bold opacity-30">Wait for your next affirmation notification</p>}
     </>)
